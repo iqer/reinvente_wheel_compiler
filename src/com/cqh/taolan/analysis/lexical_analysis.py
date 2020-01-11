e76a494faa02b4fe2def89c1f7e5caec5e59a14a
ArthurChen <cqh0718@gmail.com>
2020-01-11 12:52:33 +0800

    add lexical  analysis

---
+++ src/com/cqh/taolan/analysis/lexical_analysis.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class LexicalAnalysis:
+    """
+    Lexical part
+    """
+    
+    normal = 'normal'
+    indentifer = 'indentifer'
+    sign = 'sign'
+    annotation = 'annotation'
+    string = 'string'
+    regex = 'regex'
+    space = 'space'
+
+    def lexical_analysis(self):
+        pass
+
+    def read(self):
+        """
+        return Token
+        """
+        pass
